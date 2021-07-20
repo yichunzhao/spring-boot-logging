cb7861039b7bc0da047f34bc3d8a95e7aff8db71
Yichun Zhao <zhaoyichun@hotmail.com>
2021-07-20 20:21:12 +0200

    SpringBoot Logback Configuration Logging

--- src/test/java/com/ynz/demo/springbootlogging/BootLoggingApplicationTests.java
+++ src/test/java/com/ynz/demo/springbootlogging/BootLoggingApplicationTests.java
@@ -1,13 +1,39 @@
 package com.ynz.demo.springbootlogging;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BootLoggingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void contextLoads() {
+        assertThat(applicationContext, Matchers.notNullValue());
+        assertAll(
+                () -> assertThat(applicationContext, Matchers.notNullValue()),
+                () -> assertThat(environment, Matchers.notNullValue()));
+    }
+
+    @Test
+    void DataSourceUrl_ShouldPointingToH2Mem() {
+        String actual = environment.getProperty("spring.datasource.url");
+        assertNotNull(actual);
+        assertEquals("jdbc:h2:mem:db", actual);
+    }
 
 }
